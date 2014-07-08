872824da42765a6286fd5d2343e819ef4aa74126
Sameen Awan <awan@g.harvard.edu>
2014-07-08 06:47:57 -0400

    final copy

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
+
 <!DOCTYPE html>
 <html>
 <head>
 
 	<title>P2 XKCD Password Generator</title>
 	<meta charset="utf-8">
-	// <?php require 'logic.php'; ?>
+	 <?php require 'logic.php'; ?>
 
 </head>
 
@@ -12,50 +13,31 @@
 
 
 <?php
-// check if form has been submitted
-if (isset($_GET['generate'])) {
-	// how many words requested - default value is 4
-	$howManyWords=$_GET['howManyWords'];
-	// check if we should include characters or digits - default value is true
-	$includeCharacters=$_GET['includeCharacters'];
-	// store the dictionary file into an array
-	$dictionary = file('dictionary.txt');
-	// pull a random key from the dictionary array
-	$randomKey = array_rand($dictionary, $howManyWords);
-	// loop over the number of words requested
-	for ($i=0;$i<$howManyWords;$i++) {
-		// bold odd numbered words to increase readability
-		if ($i % 2 == 1) {	
-			// trim space from the word in case there is space
-			$randomStr .= "<strong>".trim($dictionary[$randomKey[$i]])."</strong>";
-		} else {
-			$randomStr .= trim($dictionary[$randomKey[$i]]);
-		}
-	}
-	if ($includeCharacters=="yes") {
-		// pull random digit or symbol from this string
-		$characters="1234567890!@#$%^&*()_+";
-		$randomChar = $characters[rand(0, strlen($characters)-1)];
-		$randomStr.=$randomChar;
-	}
-	// output
-	echo $randomStr;
-} else {
+
+
+
 	echo "<form>";
 	echo "<input type='hidden' name='generate' />";
 		echo "<table>";
+
 			echo "<tr>";
 				echo "<td>How Many Words?</td>";
 				echo "<td><input type='text' name='howManyWords' value='4'/></td>";
 			echo "</tr>";
+
 				echo "<td><div align='right'><input type='checkbox' name='includeCharacters' value='yes' checked/></div></td>";
 				echo "<td>Include Characters in the Password?</td>";		
+
+				echo "<td><div align='right'><input type='checkbox' name='includeNumbers' value='yes' checked/></div></td>";
+				echo "<td>Include Numbers in the Password?</td>";		
+
 			echo "<tr>";
 				echo "<td colspan='2' align='right'><input type='submit' value='Generate Random Password'/></td>";
-			echo "</tr>";						
+			echo "</tr>";	
+					
 		echo "</table>";
 	echo "</form>";
-}
+
 ?>
 		
 	</body>
--- logic.php
+++ logic.php
@@ -1,46 +1,44 @@
 <?php
-/*
-Initiate a blank contestants array
-We'll be filling this up with data from the $_POST array
-*/
-$contestants = Array();
- 
-/*
-Here's an example of what the $_POST Array looks like:
-Array ( 
-	[contestant0] => Alisha 
-	[contestant1] => Ben 
-	[contestant2] => Jill 
-	[contestant3] => Francis )
-*/
- 
-# This variable will be used for our bonus features to determine of a) there is no winner or if there is b) how many winners there are
-$winner_count = 0;
- 
-# Use a foreach loop to loop through the contestants array
-foreach($_POST as $key => $value) {
-	
-	/*
-	Create a variable $coin_flip; set this variable to be either 0 or 1 (i.e. heads or tails)
-	Use PHP's rand() function to randomly pick the 0/1
-	*/
-	$coin_flip = rand(0,1);
-	
-	# If the $coin_flip was 1...
-	if($coin_flip == 1) {
- 
-		# This contestant (i.e. $contestant[$value]) is set to be a "Winner"
-		$contestants[$value] = 'Winner';
- 
-		# Increment the winner count
-		$winner_count++;
-		
+
+// check if form has been submitted
+if (isset($_GET['generate'])) {
+	// how many words requested - default value is 4
+	$howManyWords=$_GET['howManyWords'];
+	// check if we should include special characters - default value is true
+	$includeCharacters=$_GET['includeCharacters'];
+	// check if we should include numbers - default value is true
+	$includeNumbers=$_GET['includeNumbers'];
+
+
+
+
+
+
+
+	// store the dictionary file into an array
+	$dictionary = file('dictionary.txt');
+
+	// pull a random key from the dictionary array
+	$randomKey = array_rand($dictionary, $howManyWords);
+
+	// loop over the number of words requested
+	for ($i=0;$i<$howManyWords;$i++) {
+			$randomStr .= ($dictionary[$randomKey[$i]]);
 	}
-	# Otherwise...
-	else {
- 
-		# This contestant (i.e $contestant[$value]) is set to be a "Loser"
-		$contestants[$value] = 'Loser';
+	if ($includeCharacters=="yes") {
+		// pull random digit or symbol from this string
+		$characters="!@#$%^&*()_+";
+		$randomChar = $characters[rand(0, strlen($characters)-1)];
+		$randomStr.=$randomChar;
 	}
- 
-} # End of loop
+
+	if ($includeNumbers=="yes") {
+		// pull random number from this string
+		$characters="1234567890";
+		$randomChar = $characters[rand(0, strlen($characters)-1)];
+		$randomStr.=$randomChar;
+	}
+
+	// output
+	echo $randomStr;
+} 
